11f79ef9c3b6f78cd45abfd8e346861c8917342a
Data708983 <2773277350@qq.com>
2026-03-17 21:18:23 +0800

    Update

--- DA.R
+++ DA.R
@@ -5,6 +5,9 @@ if(!dir.exists("./Processed"))  dir.create("./Processed")
 if(!dir.exists("./Processed/RawMatrix"))  dir.create("./Processed/RawMatrix")
 if(!dir.exists("./Processed/RawData"))  dir.create("./Processed/RawData")
 if(!dir.exists("./Processed/RawData/csv"))  dir.create("./Processed/RawData/csv")
+if(!file.exists("./Data/gdc_download_20260315_132413.267760.tar.gz")){
+  print("正在下载原文件")
+}
 tar_file = "./Data/gdc_download_20260315_132413.267760.tar.gz"
 extract_dir = "./Processed/RawMatrix"
 untar(tar_file, exdir = extract_dir)
