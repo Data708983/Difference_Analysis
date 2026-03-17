eee7d1e51f060b655d3fdd89344f48d04119844b
Data708983 <2773277350@qq.com>
2026-03-17 21:22:31 +0800

    Update

--- DA.R
+++ DA.R
@@ -6,7 +6,9 @@ if(!dir.exists("./Processed/RawMatrix"))  dir.create("./Processed/RawMatrix")
 if(!dir.exists("./Processed/RawData"))  dir.create("./Processed/RawData")
 if(!dir.exists("./Processed/RawData/csv"))  dir.create("./Processed/RawData/csv")
 if(!file.exists("./Data/gdc_download_20260315_132413.267760.tar.gz")){
-  print("正在下载原文件")
+  print("正在下载源数据\n")
+  download.file("https://github.com/Data708983/Difference_Analysis/releases/download/source/gdc_download_20260315_132413.267760.tar.gz", 
+                "./Data/gdc_download_20260315_132413.267760.tar.gz", mode = "wb")
 }
 tar_file = "./Data/gdc_download_20260315_132413.267760.tar.gz"
 extract_dir = "./Processed/RawMatrix"
